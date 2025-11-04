4018a36ce9f23d2b422b15fc524d5a0ae83b2e25
mahdi-shouib <mahdiomar402@gmail.com>
2025-11-04 22:35:33 +0200

    Add forms module

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NavBar } from './components/nav-bar/nav-bar';
 import { Footer } from './components/footer/footer';
 import { ProductsList } from './pages/products-list/products-list';
@@ -27,7 +28,8 @@ import { ProductItem } from './pages/products-list/product-item/product-item';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		NgbModule
+		NgbModule,
+		FormsModule
 	],
 	providers: [
 		provideBrowserGlobalErrorListeners(),
