607c1d814990d4edbf1eff16f19ae6c641874d31
mahdi-shouib <mahdiomar402@gmail.com>
2025-11-04 18:01:20 +0200

    Add routes for products list and cart

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsList } from './pages/products-list/products-list';
+import { Cart } from './pages/cart/cart';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: ProductsList },
+	{ path: 'cart', component: Cart }
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
