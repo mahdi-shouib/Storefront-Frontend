3e11c829715cb7507205a66b3dfd85be87be2fd7
mahdi-shouib <mahdiomar402@gmail.com>
2025-11-04 22:15:53 +0200

    Add cart functions

--- src/app/services/cart-service.ts
+++ src/app/services/cart-service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class CartService {
 
+	items: Product[] = [];
+
+	getItems(): Product[] {
+		return this.items;
+	}
+
+	addToCart(product: Product): void {
+		this.items.unshift(product);
+	}
+
+	removeFromCart(product: Product): void {
+		this.items.filter(p => p.id !== product.id);
+	}
+
+	clearCart(): Product[] {
+		this.items = [];
+		return this.items;
+	}
 }
