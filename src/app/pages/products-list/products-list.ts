6dc1311d94bd9e8b586dd8cefd79d00f734e2ea9
mahdi-shouib <mahdiomar402@gmail.com>
2025-11-07 23:25:23 +0200

    Add adding to cart functionality

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
+import { Item } from '../../models/item';
 import { ProductService } from '../../services/product-service';
+import { CartService } from '../../services/cart-service';
 
 @Component({
 	selector: 'app-products-list',
@@ -12,11 +14,20 @@ export class ProductsList implements OnInit {
 
 	products: Product[] = [];
 
-	constructor(private productService: ProductService) { }
+	constructor(private productService: ProductService, private cartService: CartService) { }
 
 	ngOnInit(): void {
 		this.productService.getProducts().subscribe(res => {
 			this.products = res;
 		})
+	}
+
+	addItem(item: Item): void {
+		if(!this.cartService.inCart(item)) {
+			this.cartService.addToCart(item);
+			alert('Item added!');
+		} else {
+			alert('Item is already in cart!');
+		}
 	}
 }
