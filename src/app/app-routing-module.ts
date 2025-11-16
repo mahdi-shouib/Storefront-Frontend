1e9230d15c920a8eff798f2fae10862bd5a20b2f
mahdi-shouib <mahdiomar402@gmail.com>
2025-11-16 21:04:49 +0200

    Add navigation to product details page

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsList } from './pages/products-list/products-list';
 import { Cart } from './pages/cart/cart';
 import { OrderSuccess } from './pages/order-success/order-success';
+import { ProductDetails } from './pages/product-details/product-details';
 
 const routes: Routes = [
 	{ path: '', component: ProductsList },
 	{ path: 'cart', component: Cart },
 	{ path: 'success', component: OrderSuccess },
+	{ path: 'product/:id', component: ProductDetails }
 ];
 
 @NgModule({
